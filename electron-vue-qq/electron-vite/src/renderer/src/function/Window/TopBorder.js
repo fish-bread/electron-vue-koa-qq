63008ea656778b9d85fbfe50592f7f001858a985
fishbread <123308170+fish-bread@users.noreply.github.com>
2025-03-24 14:59:08 +0800

    Delete electron-vue-qq directory

--- electron-vue-qq/electron-vite/src/renderer/src/function/Window/TopBorder.js
+++
@@ -1,9 +0,0 @@
-export const shrink = () => {
-  window.api.minimize()
-}
-export const setFullScreen = () => {
-  window.api.setFullScreen()
-}
-export const close = () => {
-  window.api.close()
-}
