b1040a99cdfa24fc64e15b9261958f34aa5de882
fishbread <123308170+fish-bread@users.noreply.github.com>
2025-03-30 16:10:43 +0800

    Delete electron-vue-qq directory

--- electron-vue-qq/electron-vite/src/renderer/src/function/boxclick/userminibox.js
+++
@@ -1,9 +0,0 @@
-import { ref } from 'vue'
-export const showMiniUserBox = ref(false)
-export const MiniUserBox = ref(null) // 使用 Vue 的 ref 获取 DOM 元素
-export const MiniUserBoxButton = ref(null)
-//点击用户迷你盒子
-export const UserMiniBoxClick = () => {
-  console.log('showMiniUserBox')
-  showMiniUserBox.value = !showMiniUserBox.value
-}
